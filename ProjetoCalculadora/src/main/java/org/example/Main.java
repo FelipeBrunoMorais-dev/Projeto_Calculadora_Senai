94af49cd19cfd78734507e2c7cd5c23992b46979
Braga01Martins <davimartinsbraga@gmail.com>
2026-03-12 20:55:17 -0300

    refactor: atualizacao main

--- ProjetoCalculadora/src/main/java/org/example/Main.java
+++ ProjetoCalculadora/src/main/java/org/example/Main.java
@@ -1,3 +1,8 @@
- public static void main (String[] args){
+import projetocalculadora.ProjetoCalculadora;
 
+import javax.swing.*;
+
+public static void main (String[] args){
+  ProjetoCalculadora app = new ProjetoCalculadora();
+  app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  }
