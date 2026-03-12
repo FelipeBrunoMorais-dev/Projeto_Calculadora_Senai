c88d334b83a112623899cdc0163e00317966519e
Braga01Martins <davimartinsbraga@gmail.com>
2026-03-12 20:52:17 -0300

    feat: criacao main

---
+++ ProjetoCalculadora/src/main/java/projetocalculadora/PrincipalTeste.java
@@ -0,0 +1,10 @@
+package projetocalculadora;
+
+import javax.swing.*;
+
+public class PrincipalTeste {
+    static void main() {
+        ProjetoCalculadora app = new ProjetoCalculadora();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
