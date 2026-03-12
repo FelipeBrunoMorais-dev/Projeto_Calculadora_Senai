13caa6e7bf2328e93f79fa30e3f298e5a78f6bea
Aluno <guilherme-jesus-student>
2026-03-12 20:44:57 -0300

    Comentario-gui

--- ProjetoCalculadora/src/main/java/org/example/Main.java
+++ ProjetoCalculadora/src/main/java/org/example/Main.java
@@ -1,3 +1,45 @@
  public static void main (String[] args){
 
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //echo "# Main" >> README.md
+ //git init
+ //git add README.md
+ //git commit -m "first commit"
+ //git branch -M main
+ //git remote add origin https://github.com/guilherme-jesus-student/Main.git
+ //git push -u origin main
+ //git remote add origin https://github.com/guilherme-jesus-student/Main.git
+ //git branch -M main
+ //git push -u origin main
