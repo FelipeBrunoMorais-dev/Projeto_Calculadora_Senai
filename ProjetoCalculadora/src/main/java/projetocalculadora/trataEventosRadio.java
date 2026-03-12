97658ccbe1307fca4e79733b92b18370a8b6df8f
FelipeBrunoMorais-dev <felipe-bruno01@hotmail.com>
2026-03-12 20:42:50 -0300

    estruturação projeto3

--- ProjetoCalculadora/src/main/java/projetocalculadora/trataEventosRadio.java
+++ ProjetoCalculadora/src/main/java/projetocalculadora/trataEventosRadio.java
@@ -1,5 +1,19 @@
 package projetocalculadora;
 
-public class trataEventosRadio {
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class trataEventosRadio implements ItemListener {
+    public void itemStateChanged(ItemEvent event){
+        if (event.getSource() == botao2dig){
+            casasDecimais = 2;
+        }
+        if (event.getSource() == botao2dig){
+            casasDecimais = 4;
+        }
+        if (event.getSource() == botao6dig){
+            casasDecimais = 6;
+        }
+    }
 
 }
